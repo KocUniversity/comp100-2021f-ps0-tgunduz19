f6b86e47b33047f89daf885615d15b654680c8d9
TAHIR GUNDUZ <79312470+tgunduz19@users.noreply.github.com>
2021-10-06 17:55:22 +0000

    completed late because i attended class on October 4, Monday.

---
+++ main.py
@@ -0,0 +1,14 @@
+
+x = int(input("enter a number: "))
+y = int(input("enter another number: "))
+print("x**y is", x**y)
+
+import math
+
+print("log2(x) is ", math.log2(x))
+
+
+
+#my ID number
+print("71806")
+
